eb3ae6e01270a7a9a66e31bf28dc7db3fa498f56
Antoine-Lecque <antoine/lecq@gmail.com>
2021-01-04 00:56:32 +0100

    write UserEntity class

--- src/com/uca/entity/UserEntity.java
+++ src/com/uca/entity/UserEntity.java
@@ -1,4 +1,44 @@
 package com.uca.entity;
 
 public class UserEntity {
+    private String Username;
+    private String password;
+    private Boolean isBanned;
+    private Boolean isAdmin;
+
+    public UserEntity() {
+        //Ignored !
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Boolean getBanned() {
+        return isBanned;
+    }
+
+    public void setBanned(Boolean banned) {
+        isBanned = banned;
+    }
 }
