f3275039d0bd000d070f3c830cb5aff18aca92b2
Antoine-Lecque <antoine/lecq@gmail.com>
2021-01-04 02:20:37 +0100

    Testing api access to users

--- src/com/uca/dao/_Initializer.java
+++ src/com/uca/dao/_Initializer.java
@@ -11,7 +11,7 @@ public class _Initializer {
             PreparedStatement statement;
 
             //Init articles table
-            statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS articles (id int primary key auto_increment, name varchar(100), author varchar(100) REFERENCES users(username), created_at timestamp, content longnvarchar(25000)); ");
+            statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS articles (id int primary key auto_increment, name varchar(100), author varchar(100), created_at timestamp, content longnvarchar(25000)); ");
             statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS users (username varchar(100) primary key, password varchar(100), isAdmin boolean, isBanned boolean); ");
             statement.executeUpdate();
 
@@ -25,7 +25,7 @@ public class _Initializer {
             statement.executeUpdate();
 
             statement = connection.prepareStatement("INSERT INTO users(username, password, isAdmin, isBanned) VALUES(?, ?, ?, ?);");
-            statement.setString(1, "user5");
+            statement.setString(1, "user6");
             statement.setString(2, "password");
             statement.setBoolean(3, false);
             statement.setBoolean(4, false);
